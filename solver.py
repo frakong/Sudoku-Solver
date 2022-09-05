1b1a983628a9a08ea1c5189698dc82af41aecf4c
Franklin Kong <frakong@ucdavis.edu>
2022-09-05 16:41:52 -0700

    Added function to print Sudoku puzzle

---
+++ solver.py
@@ -0,0 +1,30 @@
+sudoku_puzzle = [
+  [1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [0, 2, 0, 2, 0, 2, 0, 2, 0],
+  [3, 0, 3, 0, 3, 0, 3, 0, 3],
+  [0, 4, 0, 4, 0, 4, 0, 4, 0],
+  [5, 0, 5, 0, 5, 0, 5, 0, 5],
+  [0, 6, 0, 6, 0, 6, 0, 6, 0],
+  [7, 0, 7, 0, 7, 0, 7, 0, 7],
+  [0, 8, 0, 8, 0, 8, 0, 8, 0],
+  [9, 0, 9, 0, 9, 0, 9, 0, 9]
+]
+
+def print_sudoku_puzzle(sudoku_puzzle):
+  for row_number, row in enumerate(sudoku_puzzle):
+    if row_number != 0 and row_number % 3 == 0:
+      print ("- - - - - - - - - - - -")
+      # will start a new line so we can print next row without having to increment row_number
+    for column_number, value in enumerate(row):
+      if column_number != 0 and column_number % 3 == 0:
+        print(" | ", end="")
+      if column_number != 8 and value != 0:
+        print(str(value) + " ", end="")
+      elif column_number != 8 and value == 0:
+        print("  ", end="")
+      elif column_number == 8 and value != 0:
+        print(str(value))
+      else:
+        print(" ")
+
+print_sudoku_puzzle(sudoku_puzzle)
